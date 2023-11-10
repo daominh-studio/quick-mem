9f3105332d2b192ededd0ed6586134fb27bd1e58
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-11-10 22:07:33 +0700

    auth

---
+++ app/src/main/java/com/daominh/quickmem/auth/signin/SigninActivity.java
@@ -0,0 +1,14 @@
+package com.daominh.quickmem.auth.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.daominh.quickmem.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
---
+++ app/src/main/java/com/daominh/quickmem/auth/signup/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.daominh.quickmem.auth.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.daominh.quickmem.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
