e9e2759bb6903360e545791c96c2edfc2fbc0392
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-11-13 02:40:09 +0700

    navigation

--- app/src/main/java/com/daominh/quickmem/ui/activities/MainActivity.java
+++ app/src/main/java/com/daominh/quickmem/ui/activities/MainActivity.java
@@ -1,15 +1,46 @@
 package com.daominh.quickmem.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import com.daominh.quickmem.R;
+import com.daominh.quickmem.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        setupNavigation();
+    }
 
+    private void setupNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment,
+                R.id.bookFragment,
+                R.id.libraryFragment,
+                R.id.profileFragment
+        ).build();
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
