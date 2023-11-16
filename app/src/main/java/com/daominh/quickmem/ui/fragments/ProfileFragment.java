9077779427bb4e2a63e07ac63794e64a0bb9435f
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-11-16 20:30:58 +0700

    fix and update

--- app/src/main/java/com/daominh/quickmem/ui/fragments/ProfileFragment.java
+++ app/src/main/java/com/daominh/quickmem/ui/fragments/ProfileFragment.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.daominh.quickmem.databinding.FragmentProfileBinding;
+import com.daominh.quickmem.preferen.UserSharePreferences;
 import com.daominh.quickmem.ui.activities.profile.CoursesActivity;
 import com.daominh.quickmem.ui.activities.profile.SettingsActivity;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
+    private UserSharePreferences userSharePreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,24 +31,19 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentProfileBinding.inflate(inflater,container,false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.courseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(requireActivity(), CoursesActivity.class));
-            }
-        });
-        binding.settingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(requireActivity(), SettingsActivity.class));
-            }
-        });
+
+        userSharePreferences = new UserSharePreferences(requireActivity());
+        binding.nameProfileTv.setText(userSharePreferences.getUserName());
+        Picasso.get().load(userSharePreferences.getAvatar()).into(binding.profileIv);
+
+        binding.courseBtn.setOnClickListener(view1 -> startActivity(new Intent(requireActivity(), CoursesActivity.class)));
+        binding.settingsBtn.setOnClickListener(view12 -> startActivity(new Intent(requireActivity(), SettingsActivity.class)));
     }
 }
