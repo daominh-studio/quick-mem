594bbff2f85e68d24dfed695bffc5f915d22287d
Daocon <hadao1204@gmail.com>
2023-11-18 13:55:47 +0700

    update flip card (finish) and layout bookFragment

--- app/src/main/java/com/daominh/quickmem/adapter/ViewSetAdapter.java
+++ app/src/main/java/com/daominh/quickmem/adapter/ViewSetAdapter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.daominh.quickmem.data.model.Card;
 import com.daominh.quickmem.databinding.ItemViewSetBinding;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -15,11 +18,17 @@ import java.util.ArrayList;
 public class ViewSetAdapter extends RecyclerView.Adapter<ViewSetAdapter.ViewSetViewHolder> {
     private final Context context;
     private final ArrayList<Card> cards;
-    private boolean isFlipped = false;
+    private final ArrayList<Boolean> flippedStates;
+    private boolean isAnimationRunning;
 
     public ViewSetAdapter(Context context, ArrayList<Card> cards) {
         this.context = context;
         this.cards = cards;
+        this.flippedStates = new ArrayList<>(cards.size());
+        for (int i = 0; i < cards.size(); i++) {
+            flippedStates.add(false);
+        }
+        this.isAnimationRunning = false;
     }
 
     @NonNull
@@ -37,41 +46,51 @@ public class ViewSetAdapter extends RecyclerView.Adapter<ViewSetAdapter.ViewSetV
         holder.binding.termTv.setText(card.getFront());
         holder.binding.definitionTv.setText(card.getBack());
 
+        boolean isFlipped = flippedStates.get(position);
+
+        if (isFlipped) {
+            holder.binding.termTv.setVisibility(View.GONE);
+            holder.binding.definitionTv.setVisibility(View.VISIBLE);
+            holder.binding.definitionTv.setRotationX(-180);
+        } else {
+            holder.binding.termTv.setVisibility(View.VISIBLE);
+            holder.binding.definitionTv.setVisibility(View.GONE);
+            holder.binding.termTv.setRotationX(0);
+        }
+
         holder.binding.cardView.setOnClickListener(v -> {
-            if (!isFlipped) {
-                // If not flipped, perform a 180-degree rotation animation
-                holder.binding.cardView.animate()
-                        .rotationXBy(180)
-                        .setDuration(350)
-                        .withEndAction(() -> {
-                            // When the animation is complete, display the back content
-                            holder.binding.termTv.setVisibility(View.GONE);
-                            holder.binding.definitionTv.setVisibility(View.VISIBLE);
-                            holder.binding.definitionTv.setRotationX(-180); // Reverse the rotation of the text
-                            isFlipped = true; // Update the flipped state to true
-                        })
-                        .start();
-            } else {
-                // If already flipped, perform a rotation animation to return to the original position
-                holder.binding.cardView.animate()
-                        .rotationXBy(-180)
-                        .setDuration(350)
-                        .withEndAction(() -> {
-                            // When the animation is complete, display the front content
-                            holder.binding.termTv.setVisibility(View.VISIBLE);
-                            holder.binding.definitionTv.setVisibility(View.GONE);
-                            holder.binding.termTv.setRotationX(0); // Reset the rotation of the text
-                            isFlipped = false; // Update the flipped state to false
-                        })
-                        .start();
+            if (!isAnimationRunning) { // Check if animation is not running
+                boolean currentFlippedState = flippedStates.get(position);
+
+                if (!currentFlippedState) {
+                    isAnimationRunning = true; // Set animation state as true
+                    holder.binding.cardView.animate()
+                            .rotationXBy(180)
+                            .setDuration(350)
+                            .withEndAction(() -> {
+                                flippedStates.set(position, true);
+                                holder.binding.termTv.setVisibility(View.GONE);
+                                holder.binding.definitionTv.setVisibility(View.VISIBLE);
+                                holder.binding.definitionTv.setRotationX(-180);
+                                isAnimationRunning = false; // Reset animation state to false
+                            })
+                            .start();
+                } else {
+                    isAnimationRunning = true; // Set animation state as true
+                    holder.binding.cardView.animate()
+                            .rotationXBy(-180)
+                            .setDuration(350)
+                            .withEndAction(() -> {
+                                flippedStates.set(position, false);
+                                holder.binding.termTv.setVisibility(View.VISIBLE);
+                                holder.binding.definitionTv.setVisibility(View.GONE);
+                                holder.binding.termTv.setRotationX(0);
+                                isAnimationRunning = false; // Reset animation state to false
+                            })
+                            .start();
+                }
             }
         });
-
-        holder.binding.termTv.setVisibility(View.VISIBLE);
-        holder.binding.definitionTv.setVisibility(View.GONE);
-        holder.binding.termTv.setRotationX(0); // Reset the rotation of the text
-        holder.binding.definitionTv.setRotationX(-180); // Reverse the rotation of the text
-        isFlipped = false;
     }
 
     @Override
