c1d7e1cc890e80562bb3c6a437e29c81199bd4be
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-11-22 13:46:02 +0700

    fix crash

--- app/src/main/java/com/daominh/quickmem/data/dao/GroupDAO.java
+++ app/src/main/java/com/daominh/quickmem/data/dao/GroupDAO.java
@@ -10,44 +10,22 @@ import com.daominh.quickmem.data.model.Group;
 import java.util.ArrayList;
 
 public class GroupDAO {
-    private final QMDatabaseHelper qmDatabaseHelper;
+    QMDatabaseHelper qmDatabaseHelper;
+    SQLiteDatabase sqLiteDatabase;
 
     public GroupDAO(Context context) {
         qmDatabaseHelper = new QMDatabaseHelper(context);
     }
 
+    //insert class
     public long insertGroup(Group group) {
-        SQLiteDatabase sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
-        ContentValues contentValues = createContentValues(group);
-        long result = sqLiteDatabase.insert(QMDatabaseHelper.TABLE_CLASSES, null, contentValues);
-        sqLiteDatabase.close();
-        return result;
-    }
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
 
-    public ArrayList<Group> getClassesOwnedByUser(String id) {
-        return getGroupsByQuery(QMDatabaseHelper.TABLE_CLASSES, "user_id = ?", new String[]{id});
-    }
+        long result = 0;
 
-    public ArrayList<Group> getClassesUserIsMemberOf(String id) {
-        String query = "SELECT classes.* FROM " + QMDatabaseHelper.TABLE_CLASSES + " INNER JOIN " + QMDatabaseHelper.TABLE_CLASSES_USERS + " ON " +
-                QMDatabaseHelper.TABLE_CLASSES + ".id = " + QMDatabaseHelper.TABLE_CLASSES_USERS + ".class_id WHERE " + QMDatabaseHelper.TABLE_CLASSES_USERS + ".user_id = ?";
-        return getGroupsByQuery(query, id, new String[]{id});
-    }
-
-    public int getNumberMemberInClass(String id) {
-        return getCountByQuery(QMDatabaseHelper.TABLE_CLASSES_USERS, ".class_id = ?", new String[]{id});
-    }
-
-    public int getNumberFlashCardInClass(String id) {
-        return getCountByQuery(QMDatabaseHelper.TABLE_CLASSES_FLASHCARDS, ".class_id = ?", new String[]{id});
-    }
-
-    public ArrayList<Group> getAllClasses() {
-        return getGroupsByQuery(QMDatabaseHelper.TABLE_CLASSES, null, null);
-    }
-
-    private ContentValues createContentValues(Group group) {
         ContentValues contentValues = new ContentValues();
+
+        //put
         contentValues.put("id", group.getId());
         contentValues.put("name", group.getName());
         contentValues.put("description", group.getDescription());
@@ -55,45 +33,122 @@ public class GroupDAO {
         contentValues.put("created_at", group.getCreated_at());
         contentValues.put("updated_at", group.getUpdated_at());
         contentValues.put("status", group.getStatus());
-        return contentValues;
+
+        //insert
+        try {
+            result = sqLiteDatabase.insert(QMDatabaseHelper.TABLE_CLASSES, null, contentValues);
+        } finally {
+            sqLiteDatabase.close();
+        }
+        return result;
     }
 
-    private ArrayList<Group> getGroupsByQuery(String query, String selection, String[] selectionArgs) {
+    //get a classes owner by user_id
+    public ArrayList<Group> getClassesOwnedByUser(String id) {
         ArrayList<Group> groups = new ArrayList<>();
-        SQLiteDatabase sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.query(query, null, selection, selectionArgs, null, null, null);
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.query(QMDatabaseHelper.TABLE_CLASSES, null, "user_id = ?", new String[]{id}, null, null, null);
         if (cursor.moveToFirst()) {
             do {
-                groups.add(createGroupFromCursor(cursor));
+                Group group = new Group();
+                group.setId(cursor.getString(0));
+                group.setName(cursor.getString(1));
+                group.setDescription(cursor.getString(2));
+                group.setUser_id(cursor.getString(3));
+                group.setCreated_at(cursor.getString(4));
+                group.setUpdated_at(cursor.getString(5));
+                group.setStatus(cursor.getInt(6));
+                groups.add(group);
             } while (cursor.moveToNext());
         }
         cursor.close();
         sqLiteDatabase.close();
+
         return groups;
+
     }
 
-    private Group createGroupFromCursor(Cursor cursor) {
-        Group group = new Group();
-        group.setId(cursor.getString(0));
-        group.setName(cursor.getString(1));
-        group.setDescription(cursor.getString(2));
-        group.setUser_id(cursor.getString(3));
-        group.setCreated_at(cursor.getString(4));
-        group.setUpdated_at(cursor.getString(5));
-        group.setStatus(cursor.getInt(6));
-        return group;
-    }
-
-    private int getCountByQuery(String table, String selection, String[] selectionArgs) {
-        int count = 0;
-        SQLiteDatabase sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
-        String query = "SELECT COUNT(*) FROM " + table + " WHERE " + selection;
-        Cursor cursor = sqLiteDatabase.rawQuery(query, selectionArgs);
+    //get classes user is member of
+    public ArrayList<Group> getClassesUserIsMemberOf(String id) {
+        ArrayList<Group> classes = new ArrayList<>();
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
+        String query = "SELECT classes.* FROM " + QMDatabaseHelper.TABLE_CLASSES + " INNER JOIN " + QMDatabaseHelper.TABLE_CLASSES_USERS + " ON " +
+                QMDatabaseHelper.TABLE_CLASSES + ".id = " + QMDatabaseHelper.TABLE_CLASSES_USERS + ".class_id WHERE " + QMDatabaseHelper.TABLE_CLASSES_USERS + ".user_id = ?";
+        Cursor cursor = sqLiteDatabase.rawQuery(query, new String[]{id});
         if (cursor.moveToFirst()) {
-            count = cursor.getInt(0);
+            do {
+                Group group = new Group();
+                group.setId(cursor.getString(0));
+                group.setName(cursor.getString(1));
+                group.setDescription(cursor.getString(2));
+                group.setUser_id(cursor.getString(3));
+                group.setCreated_at(cursor.getString(4));
+                group.setUpdated_at(cursor.getString(5));
+                group.setStatus(cursor.getInt(6));
+                classes.add(group);
+            } while (cursor.moveToNext());
         }
         cursor.close();
         sqLiteDatabase.close();
-        return count;
+        return classes;
+
     }
+
+    //get number member in class by id class
+    public int getNumberMemberInClass(String id) {
+        int number = 0;
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
+        String query = "SELECT COUNT(*) FROM " + QMDatabaseHelper.TABLE_CLASSES_USERS + " WHERE " + QMDatabaseHelper.TABLE_CLASSES_USERS + ".class_id = ?";
+        Cursor cursor = sqLiteDatabase.rawQuery(query, new String[]{id});
+        if (cursor.moveToFirst()) {
+            do {
+                number = cursor.getInt(0);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+        return number;
+    }
+
+    //get number flashcard in class by id class
+    public int getNumberFlashCardInClass(String id) {
+        int number = 0;
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
+        String query = "SELECT COUNT(*) FROM " + QMDatabaseHelper.TABLE_CLASSES_FLASHCARDS + " WHERE " + QMDatabaseHelper.TABLE_CLASSES_FLASHCARDS + ".class_id = ?";
+        Cursor cursor = sqLiteDatabase.rawQuery(query, new String[]{id});
+        if (cursor.moveToFirst()) {
+            do {
+                number = cursor.getInt(0);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+        return number;
+    }
+
+    //get all classes
+    public ArrayList<Group> getAllClasses() {
+        ArrayList<Group> classes = new ArrayList<>();
+        sqLiteDatabase = qmDatabaseHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.query(QMDatabaseHelper.TABLE_CLASSES, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Group group = new Group();
+                group.setId(cursor.getString(0));
+                group.setName(cursor.getString(1));
+                group.setDescription(cursor.getString(2));
+                group.setUser_id(cursor.getString(3));
+                group.setCreated_at(cursor.getString(4));
+                group.setUpdated_at(cursor.getString(5));
+                group.setStatus(cursor.getInt(6));
+                classes.add(group);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        sqLiteDatabase.close();
+        return classes;
+    }
+
+
+
 }
