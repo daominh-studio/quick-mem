7b92f1f8b7971efd25b2a05eafa95d104662b69d
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2023-12-04 10:32:44 +0700

    clean code

--- app/src/main/java/com/daominh/quickmem/ui/activities/auth/signin/SignInActivity.java
+++ app/src/main/java/com/daominh/quickmem/ui/activities/auth/signin/SignInActivity.java
@@ -24,7 +24,6 @@ import com.daominh.quickmem.databinding.DialogForgotUsernameBinding;
 import com.daominh.quickmem.preferen.UserSharePreferences;
 import com.daominh.quickmem.ui.activities.MainActivity;
 import com.daominh.quickmem.ui.activities.auth.AuthenticationActivity;
-import com.daominh.quickmem.ui.activities.set.ViewSetActivity;
 import com.saadahmedsoft.popupdialog.PopupDialog;
 import com.saadahmedsoft.popupdialog.Styles;
 import com.saadahmedsoft.popupdialog.listener.OnDialogButtonClickListener;
@@ -47,122 +46,153 @@ public class SignInActivity extends AppCompatActivity {
         );
 
         //sign in by social
-        binding.googleBtn.setOnClickListener(v -> {
-//            intentToMain();
-        });
-        binding.facebookBtn.setOnClickListener(v -> {
-//            intentToMain();
-        });
-
+        setupSocialSignIn(binding);
 
         //on text changed
-        binding.emailEt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        setupTextChangedListeners(binding);
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                handleEmailTextChanged(s.toString(), binding);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                handleEmailTextChanged(s.toString(), binding);
-            }
-        });
-
-        binding.passwordEt.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                handlePasswordTextChanged(s.toString(), binding);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                handlePasswordTextChanged(s.toString(), binding);
-            }
-        });
-
-        binding.signInBtn.setOnClickListener(v -> {
-            final String email = binding.emailEt.getText().toString();
-            String password = binding.passwordEt.getText().toString();
-
-            if (handleEmailTextChanged(email, binding) && handlePasswordTextChanged(password, binding)) {
-                userDAO = new UserDAO(SignInActivity.this);
-
-                //username or email check
-                int number = 0;
-//                password = PasswordHasher.hashPassword(password);
-
-                if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    if (!userDAO.checkEmail(email)) {
-                        Log.e("SigningActivityy", "onCreateE: " + userDAO.checkEmail(email));
-                        Toast.makeText(this, "email", Toast.LENGTH_SHORT).show();
-                        binding.emailTil.setHelperText(getString(R.string.email_is_not_exist));
-                        binding.emailTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
-                        return;
-                    }
-                    if (!userDAO.checkPasswordEmail(email, password)) {
-                        binding.passwordTil.setHelperText(getString(R.string.password_is_not_correct));
-                        binding.passwordTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
-                        return;
-                    }
-                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    number = 1;
-                    if (!userDAO.checkUsername(email)) {
-                        Log.e("SigningActivityy", "onCreateU: " + userDAO.checkUsername(email));
-                        binding.emailTil.setHelperText(getString(R.string.user_is_not_exist));
-                        binding.emailTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
-                        return;
-                    }
-                    if (!userDAO.checkPasswordUsername(email, password)) {
-                        binding.passwordTil.setHelperText(getString(R.string.password_is_not_correct));
-                        binding.passwordTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
-                        return;
-                    }
-                }
-                user = getUser(number, email);
-                if (user.getStatus() == 0) {
-                    PopupDialog.getInstance(SignInActivity.this)
-                            .setStyle(Styles.FAILED)
-                            .setHeading(getString(R.string.loginFailed))
-                            .setDescription(getString(R.string.you_are_blocked))
-                            .setPopupDialogIcon(R.drawable.ic_delete)
-                            .setCancelable(true)
-                            .showDialog(new OnDialogButtonClickListener() {
-                                @Override
-                                public void onDismissClicked(Dialog dialog) {
-                                    super.onDismissClicked(dialog);
-                                    dialog.dismiss();
-                                }
-                            });
-
-                } else {
-                    userSharePreferences = new UserSharePreferences(SignInActivity.this);
-                    userSharePreferences.setRole(user.getRole());
-                    userSharePreferences.saveUser(user);
-                    userSharePreferences.setLogin(true);
-                    userSharePreferences.setUserName(user.getUsername());
-                    userSharePreferences.setAvatar(user.getAvatar());
-                    userSharePreferences.setEmail(user.getEmail());
-                    intentToMain();
-                    Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        //sign in button
+        setupSignInButton(binding);
 
         //forgot username
         binding.usernameTv.setOnClickListener(v -> openDialogUsername());
 
         //forgot password
         binding.passwordTv.setOnClickListener(v -> openDialogPassword());
+    }
+
+    private void setupSocialSignIn(ActivitySigninBinding binding) {
+        binding.googleBtn.setOnClickListener(v -> {
+            //intentToMain();
+        });
+        binding.facebookBtn.setOnClickListener(v -> {
+            //intentToMain();
+        });
+    }
+
+    private void setupTextChangedListeners(ActivitySigninBinding binding) {
+        binding.emailEt.addTextChangedListener(createEmailTextChangedListener(binding));
+        binding.passwordEt.addTextChangedListener(createPasswordTextChangedListener(binding));
+    }
+
+    private TextWatcher createEmailTextChangedListener(ActivitySigninBinding binding) {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                handleEmailTextChanged(s.toString(), binding);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                handleEmailTextChanged(s.toString(), binding);
+            }
+        };
+    }
+
+    private TextWatcher createPasswordTextChangedListener(ActivitySigninBinding binding) {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                handlePasswordTextChanged(s.toString(), binding);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                handlePasswordTextChanged(s.toString(), binding);
+            }
+        };
+    }
+
+    private void setupSignInButton(ActivitySigninBinding binding) {
+        binding.signInBtn.setOnClickListener(v -> {
+            final String email = binding.emailEt.getText().toString();
+            String password = binding.passwordEt.getText().toString();
+
+            if (handleEmailTextChanged(email, binding) && handlePasswordTextChanged(password, binding)) {
+                handleSignIn(email, password, binding);
+            }
+        });
+    }
+
+    private void handleSignIn(String email, String password, ActivitySigninBinding binding) {
+        userDAO = new UserDAO(SignInActivity.this);
+
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            handleEmailSignIn(email, password, binding);
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            handleUsernameSignIn(email, password, binding);
+        }
+    }
+
+    private void handleEmailSignIn(String email, String password, ActivitySigninBinding binding) {
+        if (!userDAO.checkEmail(email)) {
+            Log.e("SigningActivityy", "onCreateE: " + userDAO.checkEmail(email));
+            Toast.makeText(this, "email", Toast.LENGTH_SHORT).show();
+            binding.emailTil.setHelperText(getString(R.string.email_is_not_exist));
+            binding.emailTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+            return;
+        }
+        if (!userDAO.checkPasswordEmail(email, password)) {
+            binding.passwordTil.setHelperText(getString(R.string.password_is_not_correct));
+            binding.passwordTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+            return;
+        }
+        user = getUser(0, email);
+        handleUserStatus(user);
+    }
+
+    private void handleUsernameSignIn(String username, String password, ActivitySigninBinding binding) {
+        if (!userDAO.checkUsername(username)) {
+            Log.e("SigningActivityy", "onCreateU: " + userDAO.checkUsername(username));
+            binding.emailTil.setHelperText(getString(R.string.user_is_not_exist));
+            binding.emailTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+            return;
+        }
+        if (!userDAO.checkPasswordUsername(username, password)) {
+            binding.passwordTil.setHelperText(getString(R.string.password_is_not_correct));
+            binding.passwordTil.setHelperTextColor(ColorStateList.valueOf(Color.RED));
+            return;
+        }
+        String email = userDAO.getEmailByUsername(username);
+        user = getUser(1, email);
+        handleUserStatus(user);
+    }
+
+    private void handleUserStatus(User user) {
+        if (user.getStatus() == 0) {
+            PopupDialog.getInstance(SignInActivity.this)
+                    .setStyle(Styles.FAILED)
+                    .setHeading(getString(R.string.loginFailed))
+                    .setDescription(getString(R.string.you_are_blocked))
+                    .setPopupDialogIcon(R.drawable.ic_delete)
+                    .setCancelable(true)
+                    .showDialog(new OnDialogButtonClickListener() {
+                        @Override
+                        public void onDismissClicked(Dialog dialog) {
+                            super.onDismissClicked(dialog);
+                            dialog.dismiss();
+                        }
+                    });
+
+        } else {
+            userSharePreferences = new UserSharePreferences(SignInActivity.this);
+            userSharePreferences.setRole(user.getRole());
+            userSharePreferences.saveUser(user);
+            userSharePreferences.setLogin(true);
+            userSharePreferences.setUserName(user.getUsername());
+            userSharePreferences.setAvatar(user.getAvatar());
+            userSharePreferences.setEmail(user.getEmail());
+            intentToMain();
+            Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void intentToMain() {
@@ -232,7 +262,7 @@ public class SignInActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    //show keyboard automatically
+    //show keyboard automatically email and username
 
     private void showKeyboardAutomatically(DialogForgotUsernameBinding binding) {
         binding.emailEt.postDelayed(() -> {
@@ -241,6 +271,9 @@ public class SignInActivity extends AppCompatActivity {
             binding.emailEt.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, 0, 0, 0));
         }, 100);
     }
+
+
+
 
     //check format email
     private TextWatcher createEmailTextWatcher(DialogForgotUsernameBinding binding) {
@@ -289,6 +322,9 @@ public class SignInActivity extends AppCompatActivity {
         builder.setCancelable(true);
         AlertDialog alertDialog = builder.create();
 
+        //show keyboard automatically
+        showKeyboardAutomaticallyPassword(binding);
+
         binding.cancelTv.setOnClickListener(v -> alertDialog.dismiss());
 
         binding.emailOrUsernameEt.addTextChangedListener(createEmailUserForgotPasswordTextWatcher(binding));
@@ -296,6 +332,16 @@ public class SignInActivity extends AppCompatActivity {
         binding.okTv.setOnClickListener(v -> handleOkPasswordButtonClick(binding, alertDialog));
 
         alertDialog.show();
+    }
+
+
+    //show keyboard automatically password
+    private void showKeyboardAutomaticallyPassword(DialogForGotPasswordBinding binding) {
+        binding.emailOrUsernameEt.postDelayed(() -> {
+            binding.emailOrUsernameEt.requestFocus();
+            binding.emailOrUsernameEt.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0, 0, 0));
+            binding.emailOrUsernameEt.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, 0, 0, 0));
+        }, 100);
     }
 
     private TextWatcher createEmailUserForgotPasswordTextWatcher(DialogForGotPasswordBinding binding) {
@@ -317,30 +363,33 @@ public class SignInActivity extends AppCompatActivity {
         };
     }
 
+    //handle ok button click
+
     private void handleOkPasswordButtonClick(DialogForGotPasswordBinding binding, AlertDialog alertDialog) {
         String emailOrUsername = binding.emailOrUsernameEt.getText().toString();
-        if (handleEmailUserForgotPasswordTextChanged(emailOrUsername, binding)) {
-            userDAO = new UserDAO(SignInActivity.this);
-            if (Patterns.EMAIL_ADDRESS.matcher(emailOrUsername).matches()) {
-                if (!userDAO.checkEmail(emailOrUsername)) {
-                    binding.emailOrUsernameEt.setError(getString(R.string.email_is_not_exist));
-                    return;
-                } else {
-                    binding.emailOrUsernameEt.setError(null);
-                    alertDialog.dismiss();
-                }
-                Toast.makeText(this, getString(R.string.check_your_email), Toast.LENGTH_SHORT).show();
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(emailOrUsername).matches()) {
-                if (!userDAO.checkUsername(emailOrUsername)) {
-                    binding.emailOrUsernameEt.setError(getString(R.string.user_is_not_exist));
-                    return;
-                } else {
-                    binding.emailOrUsernameEt.setError(null);
-                    alertDialog.dismiss();
-                }
-                Toast.makeText(this, getString(R.string.check_your_email), Toast.LENGTH_SHORT).show();
-            }
+        if (!handleEmailUserForgotPasswordTextChanged(emailOrUsername, binding)) {
+            return;
         }
+
+        userDAO = new UserDAO(SignInActivity.this);
+        if (Patterns.EMAIL_ADDRESS.matcher(emailOrUsername).matches()) {
+            if (!userDAO.checkEmail(emailOrUsername)) {
+                binding.emailOrUsernameEt.setError(getString(R.string.email_is_not_exist));
+                return;
+            }
+            binding.emailOrUsernameEt.setError(null);
+            alertDialog.dismiss();
+            Toast.makeText(this, getString(R.string.check_your_email), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!userDAO.checkUsername(emailOrUsername)) {
+            binding.emailOrUsernameEt.setError(getString(R.string.user_is_not_exist));
+            return;
+        }
+        binding.emailOrUsernameEt.setError(null);
+        alertDialog.dismiss();
+        Toast.makeText(this, getString(R.string.check_your_email), Toast.LENGTH_SHORT).show();
     }
 
     //check format email
